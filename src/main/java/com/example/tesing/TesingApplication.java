f2cddc218f124d420579c97414d3bb64ebd3f9f9
Disandu Sanhida Jayawardhane <disandu.20230469@iit.ac.lk>
2024-11-03 13:09:41 +0530

    first commit

---
+++ src/main/java/com/example/tesing/TesingApplication.java
@@ -0,0 +1,13 @@
+package com.example.tesing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/tesing/TesingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tesing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
